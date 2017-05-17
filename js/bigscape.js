883b4231a4a77b89a78d0795f82f13610f213662
Satria Ardhe Kautsar <satriaphd@gmail.com>
2017-05-17 19:51:09 +0200

    Fifth commit

--- js/bigscape.js
+++ js/bigscape.js
@@ -2,7 +2,7 @@ var BigscapeFunc = {
   ver: "1.0"
 };
 
-function Bigscape(bs_data, bs_families, bs_similarity, network_container, desc_container, options = {}) {
+function Bigscape(bs_data, bs_families, bs_similarity, network_container, options = {}) {
   var bigscape = this;
   var graph = Viva.Graph.graph();
   var graphics = Viva.Graph.View.svgGraphics();
@@ -10,13 +10,66 @@ function Bigscape(bs_data, bs_families, bs_similarity, network_container, desc_c
   var bs_families = bs_families;
   var bs_similarity = bs_similarity;
   var bs_to_cl = [];
-  $("#" + network_container).html("<div class='network-layer' style='height: 100%; width: 100%;'><div class='network-overlay' style='display: none; height: 100%; width: 100%; position: absolute; top: 0; left: 0;'>");
-  $("#" + network_container).css("position", "relative");
+  $("#" + network_container).html("<div class='network-layer' style='position: fixed; top: 0; left: 0; bottom: 0; right: 0;'><div class='network-overlay' style='display: none; position: fixed; top: 0; left: 0; bottom: 0; right: 0;'>");
   var net_ui = $("#" + network_container + " .network-layer");
-  var desc_ui = $("#" + desc_container);
+  var desc_ui = $("<div class='desc-ui hidden' style='margin-top: 2px;'>");
+  var desc_btn = $("<a title='Details' href='#' class='showhide-btn'></a>");
+  desc_btn.click(function(handler){
+    if ($(handler.target).hasClass("active")) {
+      $(handler.target).removeClass("active");
+      $(handler.target).parent().removeClass("active");
+      desc_ui.addClass("hidden");
+    } else {
+      $(handler.target).addClass("active");
+      $(handler.target).parent().addClass("active");
+      desc_ui.removeClass("hidden");
+    }
+  });
+  net_ui.after("<div class='desc-container'></div>");
+  net_ui.parent().find(".desc-container").append(desc_btn);
+  net_ui.parent().find(".desc-container").append(desc_ui);
+  var info_ui = $("<div class='' style='margin-top: 2px;'>");
+  var info_btn = $("<a title='Info' href='#' class='showhide-btn active hidden'></a>");
+  info_btn.click(function(handler){
+    if ($(handler.target).hasClass("active")) {
+      $(handler.target).removeClass("active");
+      info_ui.addClass("hidden");
+    } else {
+      $(handler.target).addClass("active");
+      info_ui.removeClass("hidden");
+    }
+  });
+  net_ui.after("<div class='info-container'></div>");
+  net_ui.parent().find(".info-container").append(info_btn);
+  net_ui.parent().find(".info-container").append(info_ui);
+  var nav_ui = $("<div class='' style='margin-top: 2px;'>");
+  nav_ui.append("<div class='show-singletons'></div>");
+  nav_ui.append("<div class='selection-text'></div>");
+  var nav_btn = $("<a title='Navigation' href='#' class='showhide-btn active hidden'></a>");
+  nav_btn.click(function(handler){
+    if ($(handler.target).hasClass("active")) {
+      $(handler.target).removeClass("active");
+      nav_ui.addClass("hidden");
+    } else {
+      $(handler.target).addClass("active");
+      nav_ui.removeClass("hidden");
+    }
+  });
+  net_ui.after("<div class='nav-container'></div>");
+  net_ui.parent().find(".nav-container").append(nav_btn);
+  net_ui.parent().find(".nav-container").append(nav_ui);
   var highlighted_nodes = [];
   var selected_node = -1;
   var edge_count = 0;
+  net_ui.after("<div class='navtext-container'>Shift+Drag: multi-select. Shift+Scroll: zoom in/out.</div>");
+  net_ui.after("<div class='detail-container hidden'></div>");
+  var det_ui = $("<div>");
+  var det_btn = $("<a title='Close' href='#' class='showhide-btn active'></a>");
+  det_btn.click(function(handler){
+    $(handler.target).parent().addClass("hidden");
+  });
+  net_ui.parent().find(".detail-container").append(det_btn);
+  net_ui.parent().find(".detail-container").append(det_ui);
 
   // options
   var intra_cutoff = options.intra_cutoff? options.intra_cutoff : 0;
@@ -30,7 +83,7 @@ function Bigscape(bs_data, bs_families, bs_similarity, network_container, desc_c
   this.highlightNodes = highlightNodes;
   this.setHighlightedNodes = function(ids) { highlighted_nodes = ids; };
   this.getHighlightedNodes = function() { return highlighted_nodes; };
-  var updateDescription = function(ids, id = -1) {
+  var updateDescription = function(ids = highlighted_nodes, id = selected_node) {
     if (ids.length > 0) {
       if (id > 0) {
         if (ids.indexOf(id) < 0) {
@@ -44,11 +97,12 @@ function Bigscape(bs_data, bs_families, bs_similarity, network_container, desc_c
         }
       }
     };
-    var calced = BigscapeFunc.updateDescription(id, ids, bs_data, bs_to_cl, bs_families, desc_ui, bigscape);
+    var calced = BigscapeFunc.updateDescription(id, ids, bs_data, bs_to_cl, bs_families, desc_ui, nav_ui, det_ui, bigscape);
     net_ui.find("svg circle").attr("stroke-width", "0");
     if (calced > -1) {
+      graphics.getNodeUI(calced).attr("r", "40");
       graphics.getNodeUI(calced).attr("stroke", "red");
-      graphics.getNodeUI(calced).attr("stroke-width", "5");
+      graphics.getNodeUI(calced).attr("stroke-width", "10");
     };
     return calced;
   };
@@ -125,6 +179,7 @@ function Bigscape(bs_data, bs_families, bs_similarity, network_container, desc_c
       highlightNodes(highlighted_nodes);
       selected_node = updateDescription(highlighted_nodes, new_sel);
     });
+    $(ui).rightclick
    return ui;
   }).placeNode(function(nodeUI, pos){
     nodeUI.attr('cx', pos.x).attr('cy', pos.y);
@@ -150,19 +205,22 @@ function Bigscape(bs_data, bs_families, bs_similarity, network_container, desc_c
   var renderer = Viva.Graph.View.renderer(graph, {
     container: net_ui[0],
     layout : layout,
-    graphics : graphics
+    graphics : graphics,
+    interactive: 'node drag'
   });
 
   var multiSelectOverlay;
   document.addEventListener('keydown', function(e) {
     if (e.which === 16 && !multiSelectOverlay) { // shift key
       multiSelectOverlay = BigscapeFunc.startMultiSelect(graph, graphics, renderer, layout, bigscape);
+      net_ui.css("cursor", "crosshair");
     }
   });
   document.addEventListener('keyup', function(e) {
     if (e.which === 16 && multiSelectOverlay) {
       multiSelectOverlay.destroy();
       multiSelectOverlay = null;
+      net_ui.css("cursor", "default");
     }
   });
 
@@ -173,29 +231,30 @@ function Bigscape(bs_data, bs_families, bs_similarity, network_container, desc_c
   var countDown = 5 + parseInt(graph.getLinksCount() / 1000);
   var perZoom = 5;
   var zoomCount = 0;
-  var info_ui = $("<div class='info-container' style='margin: 0.5em; height: 50px; position: fixed; background-color: white;'>");
-  net_ui.prepend(info_ui);
-  info_ui.append("<div>Adjusting network layout for... <span class='network-layout-counter'>" + countDown + "</span> second(s) </div>");
+  info_ui.append("<div>Adjusting network layout for... <span class='network-layout-counter'>" + countDown + "</span> second(s)</div>");
   var interval = setInterval(function(){
     countDown--;
-    if (countDown > 0) {
-      net_ui.find(".network-layout-counter").text(countDown);
-      var scale = 1.5 * (graphics.getSvgRoot().getElementsByTagName("g")[0].getBoundingClientRect().width / graphics.getSvgRoot().getBoundingClientRect().width);
+    if (countDown >= 0) {
+      info_ui.find(".network-layout-counter").text(countDown);
+      var scale = 3 * (graphics.getSvgRoot().getElementsByTagName("g")[0].getBoundingClientRect().width / graphics.getSvgRoot().getBoundingClientRect().width);
       var point = {x: graphics.getSvgRoot().getBoundingClientRect().width / 2,
                   y: graphics.getSvgRoot().getBoundingClientRect().height / 2};
       graphics.scale((1 / scale), point);
     } else {
-      net_ui.find(".network-layout-counter").parent().addClass("hidden");
+      info_ui.html("");
       var nodes_with_edges_count = 0;
       graph.forEachNode(function(node) {
         if (node.links.length > 0) {
           nodes_with_edges_count++;
         }
       });
-      info_ui.append("<div>Total nodes: " + graph.getNodesCount() + " (" + (graph.getNodesCount() - nodes_with_edges_count) + " singleton/s), Total links: " + graph.getLinksCount() + "</div>");
-      var checkbox = $("<div><input type='checkbox'/> Show singletons</div>");
-      checkbox.find("input[type=checkbox]").change(function() { showSingletons($(this).is(":checked")); });
-      info_ui.append(checkbox);
+      var singleton_count = (graph.getNodesCount() - nodes_with_edges_count);
+      info_ui.append("<div>Total BGCs: " + graph.getNodesCount() + " (" + singleton_count + " singleton/s), links: " + graph.getLinksCount() + ", families: " + bs_families.length + "</div>");
+      if (singleton_count > 0) {
+        var checkbox = $("<div><input type='checkbox'/>Singletons</div>");
+        checkbox.find("input[type=checkbox]").change(function() { showSingletons($(this).is(":checked")); });
+        nav_ui.find(".show-singletons").html(checkbox);
+      }
       renderer.pause();
       clearInterval(interval);
     }}, 1000);
@@ -218,31 +277,83 @@ BigscapeFunc.showSingletons = function(graph, graphics, net_ui, isOn) {
 }
 
 // ...
-BigscapeFunc.updateDescription = function(id_sel, ids, bs_data, bs_to_cl, bs_families, desc_ui, bigscape) {
+BigscapeFunc.updateDescription = function(id_sel, ids, bs_data, bs_to_cl, bs_families, desc_ui, nav_ui, det_ui, bigscape) {
   desc_ui.html("");
   if (ids.length > 0) {
+    // ...
+    var top = $("<div style='height: 200px; width: 100%; border: 1px solid black;'>");
+    // ...
+    var bot = $("<div>");
     var ul = $("<ul>");
     if (ids.indexOf(id_sel) < 0) {
       id_sel = ids[0];
     }
+    var sel_fam = [];
+    var fam_sels = {};
     for (var i in ids) {
       var id = ids[i];
-      var li = $("<li>");
-      li.append("<a>" + bs_data[id]["id"] + "</a>");
-      if (id == id_sel) {
-        li.append("*");
+      if (sel_fam.indexOf(bs_to_cl[id]) < 0) {
+        sel_fam.push(bs_to_cl[id]);
+        fam_sels[bs_to_cl[id]] = [id];
+      } else {
+        fam_sels[bs_to_cl[id]].push(id);
       }
-      li.find("a").click({bigscape: bigscape, id: id, ids: ids}, function(handler){
-        handler.data.bigscape.setSelectedNode(handler.data.id);
-        handler.data.bigscape.updateDescription(handler.data.ids);
-      });
+    }
+    sel_fam.sort(function(a, b) { // why not working??
+      var id1 = bs_families[a]["id"].toUpperCase();
+      var id2 = bs_families[b]["id"].toUpperCase();
+      if (id1 < id2) { return -1; }
+      else if (id2 > id1) { return 1; }
+      else { return 0; }
+    });
+    for (var i in sel_fam) {
+      var li = $("<li>");
+      li.text(bs_families[sel_fam[i]]["id"]);
+      var ull = $("<ul>");
+      for (var j in fam_sels[sel_fam[i]]) {
+        var id = fam_sels[sel_fam[i]][j];
+        var lii = $("<li>");
+        lii.append("<a>" + bs_data[id]["id"] + "</a>");
+        if (id == id_sel) {
+          lii.append("*");
+          top.append("<h3>" + bs_data[id]["id"] + "</h3>");
+          var sdbtn = $("<button>detail</button>");
+          sdbtn.click({id: id, bs_data: bs_data, bs_families: bs_families, bs_to_cl: bs_to_cl, det_ui: det_ui}, function(handler){
+            BigscapeFunc.openDetail(handler.data.id, handler.data.bs_data, handler.data.bs_to_cl, handler.data.bs_families, handler.data.det_ui);
+            handler.data.det_ui.parent().removeClass("hidden");
+          });
+          top.append(sdbtn);
+        }
+        lii.find("a").click({bigscape: bigscape, id: id, ids: ids}, function(handler){
+          handler.data.bigscape.setSelectedNode(handler.data.id);
+          handler.data.bigscape.updateDescription(handler.data.ids);
+        });
+        ull.append(lii);
+      }
+      li.append(ull);
       ul.append(li);
     }
-    desc_ui.append("<div>Selected: " + ids.length + " BGC" + (ids.length > 1? "s":"") + "</div>");
-    desc_ui.append(ul);
+    var clearSel = $("<a href='#'>clear</a>");
+    clearSel.click({bigscape: bigscape}, function(handler){
+      handler.data.bigscape.setHighlightedNodes([]);
+      handler.data.bigscape.setSelectedNode(-1);
+      handler.data.bigscape.highlightNodes([]);
+      handler.data.bigscape.updateDescription();
+    });
+    nav_ui.find(".selection-text").text(
+      "Selected: " + ids.length + " BGC" + (ids.length > 1? "s":"")
+      + ", " + sel_fam.length +  " Famil" + (sel_fam.length > 1? "ies":"y")
+      + " ("
+    );
+    nav_ui.find(".selection-text").append(clearSel);
+    nav_ui.find(".selection-text").append(")");
+    bot.append(ul);
+    // ...
+    desc_ui.append(top);
+    desc_ui.append(bot);
   } else {
     id_sel = -1;
-    desc_ui.append("Hover on one of the node to see its description, click on it to lock selection.");
+    nav_ui.find(".selection-text").text("");
   }
   return id_sel;
 }
@@ -262,6 +373,14 @@ BigscapeFunc.highlightNodes = function(graph, graphics, ids, bs_data, bs_to_cl, 
         }
     });
   }
+}
+
+// ...
+BigscapeFunc.openDetail = function(id, bs_data, bs_to_cl, bs_families, det_ui) {
+  det_ui.html("");
+  det_ui.append("<h2>" + bs_data[id]["id"] + "<h2>");
+  det_ui.append("<object data='../../cluster_svg/" + bs_data[id]["id"] + ".svg' type='image/svg+xml'>");
+  //det_ui.append("<img src='../../cluster_svg/" + bs_data[id]["id"] + ".svg'>");
 }
 
 // --- highlighter ---
